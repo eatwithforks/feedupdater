e69225e2e3415e6c5f97db42a976d20af45b6530
Jye Lee <jlee@cloudpassage.com>
2014-10-22 09:53:21 -0700

    removed redundant logic that separates first image from rest

--- fetcher.rb
+++ fetcher.rb
@@ -19,28 +19,16 @@ List.each do |manga|
   results = agent.page.links_with(:text => /#{manga}.*\s\d/)
   results.each do |release|
     page = release.click
-
     next if agent.page.links.find { |l| l.text.match(/#{Nextpage}/) }.nil?
-    page.images.each { |e| @first_image = e.url.to_s if e.url.to_s.match(/#{Pictype}/) }
-
-    img = agent.get(@first_image)
-    file_name = @first_image.split('/')[-1]
-    chapter = @first_image.split('/')[-2]
-
-    next if Dir.exists?("#{SaveDir}/#{chapter}")
-    img.save "#{SaveDir}/#{chapter}/#{file_name}"
-
-    page = agent.get(Targeturl)
-    page = release.click
 
     images = []
     loop do
       begin
-        next_link = agent.page.link_with(:text => /#{Nextpage}/)
-        page = next_link.click
-
         page.images.each { |e| @image = e.url.to_s if e.url.to_s.match(/#{Pictype}/) }
         images << @image
+
+        next_link = agent.page.link_with(:text => /#{Nextpage}/)
+        page = next_link.click
       rescue Mechanize::UnsupportedSchemeError => e
         break
       end
@@ -49,15 +37,15 @@ List.each do |manga|
     images.each do |pic|
       begin
         file_name = pic.split('/')[-1]
-        chapter = pic.split('/')[-2]
+        @chapter = pic.split('/')[-2]
         img = agent.get(pic)
-        img.save "#{SaveDir}/#{chapter}/#{file_name}"
+        img.save "#{SaveDir}/#{@chapter}/#{file_name}"
       rescue Mechanize::ResponseCodeError => e
         break
       end
     end
     updated_counter += 1
-    updated_manga << chapter
+    updated_manga << @chapter
   end
 end
 
